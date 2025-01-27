47d1d4b1d7bdb7dc19b27e32e652dec39e360a76
Mykola-Maik <mykoluamaik@gmail.com>
2025-01-27 15:25:27 +0200

    feature/nestjs-setup

---
+++ src/common/database/abstract.repository.ts
@@ -0,0 +1,58 @@
+import type { AbstractDocument } from './abstract.schema';
+import { NotFoundException, type Logger } from '@nestjs/common';
+import { Types, type Model, FilterQuery, UpdateQuery } from 'mongoose';
+
+export abstract class AbstractRepository<TDocument extends AbstractDocument> {
+  protected abstract readonly logger: Logger;
+
+  constructor(protected readonly model: Model<TDocument>) {}
+
+  async create(document: Omit<TDocument, '_id'>): Promise<TDocument> {
+    const createdDocument = new this.model({
+      ...document,
+      _id: new Types.ObjectId(),
+    });
+
+    return (await createdDocument.save()).toJSON() as unknown as TDocument;
+  }
+
+  async fineOne(filterQuery: FilterQuery<TDocument>): Promise<TDocument> {
+    // lean() returns a document without metadata, just what we have in DB
+    const document = await this.model.findOne(filterQuery).lean<TDocument>();
+
+    if (!document) {
+      this.logger.warn('Document was not found with filterQuery', filterQuery);
+      throw new NotFoundException('Document not found.');
+    }
+
+    return document;
+  }
+
+  async fineOneAndUpdate(
+    filterQuery: FilterQuery<TDocument>,
+    update: UpdateQuery<TDocument>,
+  ) {
+    const document = await this.model
+      .findOneAndUpdate(filterQuery, update, {
+        new: true,
+      })
+      .lean<TDocument>();
+
+    if (!document) {
+      this.logger.warn('Document was not found with filterQuery', filterQuery);
+      throw new NotFoundException('Document not found.');
+    }
+
+    return document;
+  }
+
+  async find(filterQuery: FilterQuery<TDocument>): Promise<TDocument[]> {
+    return this.model.find(filterQuery).lean<TDocument[]>();
+  }
+
+  async fineOneAndDelete(
+    filterQuery: FilterQuery<TDocument>,
+  ): Promise<TDocument> {
+    return this.model.findOneAndDelete(filterQuery).lean<TDocument>();
+  }
+}
